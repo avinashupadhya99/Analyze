71fc9c072a08dfc997273235f7cc912c69b26c80
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2021-07-24 15:01:32 +0530

    feat: join command

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
-import { Client } from 'discord.js';
+import { Client, GuildMember, PartialGuildMember, Speaking, VoiceChannel, VoiceConnection } from 'discord.js';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
+const PREFIX = '!';
 
 const client: Client = new Client();
 
-client.on('ready', async () => {
+client.on('ready', () => {
     console.log("Bot is alive");
 });
 
-client.on('message', (message) => {
+client.on('message', async (message) => {
     console.log(message);
+    if(message.author.bot) return;
+    if(message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content.trim().substring(PREFIX.length).split("\n");
+        switch(CMD_NAME) {
+            case 'join':
+                const channel = message.member?.voice.channel;
+                if (!channel) return message.reply("Please join a voice channel to invite the bot");
+                channel.join().then((connection: VoiceConnection) => {
+                    console.log(connection);
+                }).catch(error => {
+                    console.error(error);
+                    return message.reply("Something went wrong");
+                });
+            break;
+        }
+    }
 });
+
+client.on('guildMemberSpeaking', (member: GuildMember | PartialGuildMember, speech: Readonly<Speaking>) => {
+
+})
 
 client.login(process.env.DISCORD_BOT_TOKEN);
